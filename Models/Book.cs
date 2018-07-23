58c9fc3a7ff29f6e27eb2d1ada2a48eb7ca7d81a
cjwillis809 <cjwillis809@gmail.com>
2018-07-23 14:26:10 -0500

    Added DataAnnotations

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace bookglobe_backend.Models
 {
     public class Book
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Title { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Author { get; set; }
         public string CoverImage { get; set; }
         public int PageLength { get; set; }
